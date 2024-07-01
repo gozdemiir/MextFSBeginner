9315d9d899eec168be74914197d41cf7580c4d5e
Gamze Özdemir <gamzeozdemiir1@gmail.com>
2024-07-01 23:38:29 +0300

    [Feat] ECommerApp added

---
+++ ECommerceApp/EntityFramework/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using ECommerceApp.Entities;
+using ECommerceApp.EntityFramework.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace ECommerceApp.EntityFramework.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=ECommerceAppTunga;User Id=alper;Password=123alper123;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
